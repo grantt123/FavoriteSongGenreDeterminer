7eccac4deb0b464cec9d318f59686276b5fe5908
grant <147453883+grantt123@users.noreply.github.com>
2023-10-11 17:12:38 -0400

    Update SongGenresList.java

--- SongGenresList.java
+++ SongGenresList.java
@@ -1,3 +1,12 @@
+//*********************************************
+// Title: Favorite Song Genre Determiner
+// Name: Grant Tran
+// Course Section: CMIS202-ONL1 (Seidel) Fall 2023
+// File: FavoriteSongGenre.java
+// Description: This file is where it adapts from the SongGenrePicker class and has the certain song genres
+// where the user can choose from.
+//*********************************************
+
 import java.util.*;
 
 public class SongGenresList extends SongGenrePicker {
