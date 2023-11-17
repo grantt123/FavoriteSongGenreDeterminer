53d160b218c9abfa454b877ce7eb24686b560057
grant <147453883+grantt123@users.noreply.github.com>
2023-11-17 16:05:47 -0500

    Create HashTable.java

---
+++ HashTable.java
@@ -0,0 +1,34 @@
+//*********************************************
+// Title: Favorite Song Genre Determiner
+// Name: Grant Tran
+// Course Section: CMIS202-ONL1 (Seidel) Fall 2023
+// File: HashTable.java
+// Description: This file is the hashtable is implemented and is used in the Interface file
+//*********************************************
+
+import java.util.LinkedList;
+
+public class HashTable {
+    private LinkedList<String>[] table;
+    private static final int TABLE_SIZE = 10; 
+    public HashTable() {
+        table = new LinkedList[TABLE_SIZE];
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    public void put(String key) {
+        int hash = calculateHash(key);
+        table[hash].add(key);
+    }
+
+    public boolean contains(String key) {
+        int hash = calculateHash(key);
+        return table[hash].contains(key);
+    }
+
+    private int calculateHash(String key) {
+        return key.length() % TABLE_SIZE;
+    }
+}
